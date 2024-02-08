4413cf38808526351ae2570d8adcebc0c014e347
fvrbloom <dilnaz1327@gmail.com>
2024-02-08 17:17:56 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import Game from './components/Game';
+import './styles.css';
 
 function App() {
+  const [selectedLevel, setSelectedLevel] = useState(0);
+
+  const handleLevelClick = (level) => {
+    setSelectedLevel(level - 1); // Adjust the level to be 0-indexed
+  };
+
+  const handleLevelComplete = () => {
+    // Increment the selected level when the current level is completed
+    setSelectedLevel((prevLevel) => prevLevel + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {/* Sidebar */}
+      <div className="sidebar">
+        <h2>Levels</h2>
+        <ul>
+          {Array.from({ length: 10 }, (_, index) => (
+            <li key={index + 1} onClick={() => handleLevelClick(index + 1)}>
+              Level {index + 1}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Main Content */}
+      <div className="main-content">
+        <h1>Maqal Drop</h1>
+        <Game level={selectedLevel} onLevelComplete={handleLevelComplete} />
+      </div>
     </div>
   );
 }
